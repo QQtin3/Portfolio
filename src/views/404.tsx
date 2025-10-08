099c28e819b43518c7b8fba2c3846cb920cf93a6
QQtin <dev@qqtinworks.fr>
2025-10-08 18:05:24 +0200

    404 Page done

--- src/views/404.tsx
+++ src/views/404.tsx
@@ -1,17 +1,18 @@
 import {StarBackground} from "../components/StarBackground.tsx";
 import '../styles/404.css'
+import {Link} from "react-router-dom";
 
 function PageNotFound() {
 	return (
 		<>
 			<StarBackground/>
 			<div className={"centeredDiv"}>
-				<h1>Erreur 404</h1>
 				<h1>Oops !</h1>
-				<p>La page sur laquelle vous êtes arrivé n'existe pas</p>
-				<button>Retour vers le menu principal</button>
+				<p>La page que vous cherchez n'existe pas</p>
+				<Link to="/">
+					<button>Retour au menu principal</button>
+				</Link>
 			</div>
-
 		</>
 	)
 }
